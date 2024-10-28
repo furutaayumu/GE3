70a62d0199b8df7c761896cd69aa1891c770f4aa
furutaayumu <k023g0086@g.neec.ac.jp>
2024-10-28 14:50:29 +0900

    CG2

---
+++ resources/shaders/particle.VS.hlsl
@@ -0,0 +1,24 @@
+#include "particle.hlsli"
+
+
+
+struct TransformationMatrix {
+	float4x4 WVP;
+};
+ConstantBuffer<TransformationMatrix> gTransformationMatrix:register(b0);
+
+
+
+struct VertexShaderInput {
+	float4 position : POSITION0;
+    float2 texcoord : TEXCOORD0;
+};
+
+VertexShaderOutput main(VertexShaderInput input) {
+	VertexShaderOutput output;
+	output.position = mul(input.position, gTransformationMatrix.WVP) ;//output.position = input.position
+    output.texcoord = input.texcoord;
+	return output;
+}
+
+
