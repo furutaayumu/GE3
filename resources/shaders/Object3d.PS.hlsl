3fcdd92f58166064056aa79deea03acc6cde2bea
furutaayumu <k023g0086@g.neec.ac.jp>
2025-02-14 12:40:17 +0900

    kan

--- resources/shaders/Object3d.PS.hlsl
+++ resources/shaders/Object3d.PS.hlsl
@@ -1,28 +1,51 @@
-#include "object3d.hlsli"
+#include"object3d.hlsli"
+struct Pixelshaderoutput {
+float4 color:SV_TARGET0;
 
-struct Pixelshaderoutput
-{
-    float4 color : SV_TARGET0;
+};
+Texture2D<float4>gTexture:register(t0);
+SamplerState gSampler:register(s0);
+struct Material {
+	float4 color;
+	int32_t enableLighting;
 };
 
-struct Material
+struct DirectionalLight
 {
-    float4 color;
+	float32_t4 color;//!ライトの色
+	float32_t3 direction;//!<ライトの向き
+	float intensity;//!<輝度
 };
-ConstantBuffer<Material> gMaterial : register(b0);
-Texture2D<float4> gTexture : register(t0);
-SamplerState gSampler : register(s0);
 
-Pixelshaderoutput main(VertexShaderOutput input)
-{
-    Pixelshaderoutput output;
-    
-    /*output.color = gMaterial.color; *///float32_t4(1.0, 1.0, 1.0, 1.0);
-    
-   
+ConstantBuffer<Material>gMaterial:register(b0);
 
-    float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    output.color = gMaterial.color * textureColor;
-    return output;
+ConstantBuffer<DirectionalLight>gDirectionalLight:register(b1);
+
+
+Pixelshaderoutput main(VertexShaderOutput input) {
+	Pixelshaderoutput output;
+	output.color = gMaterial.color; //float32_t4(1.0, 1.0, 1.0, 1.0);
+	float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+	
+	if (gMaterial.enableLighting != 0)
+	{//Lightingする場合
+		//float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+		float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+	}
+	else
+	{//Lightingしない場合。前回までと同じ演算
+		output.color = gMaterial.color * textureColor;
+	}
+
+	return output;
 }
+
+
+
+
+
+
+
 
--- resources/shaders/Object3d.hlsli
+++ resources/shaders/Object3d.hlsli
@@ -1,5 +1,6 @@
 struct VertexShaderOutput
 {
-    float4 position : SV_POSITION;
-    float2 texcoord : TEXCOORD0;
+	float4 position : SV_POSITION;
+	float2 texcoord : TEXCOORD0;
+	float32_t3 normal : NORMAL0;
 };
