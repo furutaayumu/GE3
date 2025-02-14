40c5a2a9661dd4fb6e372fa7af5c78e5770074b8
furutaayumu <k023g0086@g.neec.ac.jp>
2025-02-14 12:34:27 +0900

    kanryo

--- resources/shaders/Object3d.VS.hlsl
+++ resources/shaders/Object3d.VS.hlsl
@@ -1,22 +1,25 @@
-#include "object3d.hlsli"
-
-
+#include"object3d.hlsli"
 struct TransformationMatrix {
 	float4x4 WVP;
+	float32_t4x4 World;
 };
 ConstantBuffer<TransformationMatrix> gTransformationMatrix:register(b0);
 
-
+//struct VertexShaderOutput {
+//	float32_t4 position : SV_POSITION;
+//};
 
 struct VertexShaderInput {
-	float4 position : POSITION0;
-    float2 texcoord : TEXCOORD0;
+	float32_t4 position : POSITION0;
+	float32_t2 texcoord : TEXCOORD0;
+	float32_t3 normal : NORMAL0;
 };
 
 VertexShaderOutput main(VertexShaderInput input) {
 	VertexShaderOutput output;
-	output.position = mul(input.position, gTransformationMatrix.WVP) ;//output.position = input.position
-    output.texcoord = input.texcoord;
+	output.position =mul(input.position, gTransformationMatrix.WVP) ;//output.position = input.position
+	output.texcoord = input.texcoord;
+	output.normal = normalize(mul(input.normal, (float32_t3x3)gTransformationMatrix.World));
 	return output;
 }
 
